6b4f4c4126a7a0b4ca324dbc8f63f1cb6fdeb0db
Guy Arieli <guyarieli1997@gmail.com>
2018-12-17 00:02:16 +0200

    MT 1.41 - added some behavior to exercise section

--- learning-section/learn.js
+++ learning-section/learn.js
@@ -1,4 +1,4 @@
-
+// sidebar behavior
 $(document).ready(function() {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
@@ -9,3 +9,40 @@ $(document).ready(function() {
 $(".dropdown-toggle").click(function() {
   $(".fas").toggleClass("fas-turned");
 });
+
+// exercise toggler
+$(document).ready(function() {
+  $(".exercise").hide();
+});
+
+// generates a random question based on the number clicked
+function questionGenerator(num) {
+  var randomNumber = Math.floor(Math.random() * 10 + 1);
+  var question = num + " X " + randomNumber;
+  $(".question").text(question);
+}
+
+var currentNumberExercise;
+
+// showcase the question when number is clicked
+$("#pageSubmenu a").on("click", function() {
+  if (this == currentNumberExercise) {
+    var answer = confirm("?אתה בטוח שאתה רוצה לצאת");
+    if (answer) {
+      exerciseGenerator(this);
+    }
+  } else {
+    exerciseGenerator(this);
+  }
+});
+
+
+function exerciseGenerator(thisButton) {
+
+  currentNumberExercise = thisButton;
+  $(thisButton).toggleClass("button-clicked");
+  $(".exercise").show();
+  var numberClicked = $(thisButton).attr("class");
+  numberClicked = parseInt(numberClicked.slice(9));
+  questionGenerator(numberClicked);
+}
---
+++ test-section/test.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+    $(this).toggleClass('active');
+  });
+});
+
+$(".dropdown-toggle").click(function() {
+  $(".fas").toggleClass("fas-turned");
+});
