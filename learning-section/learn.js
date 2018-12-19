84f1bee0583b97bbee375b6b6ac6d6666e2334ab
Guy Arieli <guyarieli1997@gmail.com>
2018-12-19 16:45:15 +0200

    MT 1.5 - added styles and sounds to exercise section

--- learning-section/learn.js
+++ learning-section/learn.js
@@ -33,6 +33,8 @@ function randomNumberGenerator(min, max) {
 var currentNumberExercise;
 var exerciseClass;
 var answer;
+var answerNumber;
+var userAnswer;
 
 // starts the exercise mode
 function startExercise() {
@@ -56,23 +58,111 @@ function questionGenerator() {
   var randomNumber = randomNumberGenerator(1, 10);
   var question = numberClicked + " X " + randomNumber;
   answer = randomNumber * numberClicked;
-  buttonsAnswers();
   $(".question").text(question);
+  buttonsAnswers();
+  checkAnswer();
+
 }
 
+// adds the answers to the buttons
 function buttonsAnswers() {
-  var answerNumber = randomNumberGenerator(1, 4);
-  console.log(answerNumber);
+  // adds the corrent answer to a random number
+  answerNumber = randomNumberGenerator(1, 4);
   $(".button-" + answerNumber).text(answer);
-  for (var i = 1; i <= 4; i++) {
-    if (i != answerNumber) {
-      var randomAnswer;
-      if (answer <= 15) {
-        randomAnswer = randomNumberGenerator(1, (answer + 15));
-      } else {
-        randomAnswer = randomNumberGenerator((answer - 15), (answer + 15));
-      }
-      $(".button-" + i).text(randomAnswer);
+  // creates a new SET of 4 numbers in the range wanted
+  var buttons = new Set();
+  var randomAnswer;
+  for (var i = 0; i <= 100; i++) {
+    if (answer <= 20) {
+      randomAnswer = randomNumberGenerator(1, (answer + 20));
+    } else {
+      randomAnswer = randomNumberGenerator((answer - 20), (answer + 20));
     }
+    // checks that the new number is different than the answer
+    if (randomAnswer != answer) {
+      buttons.add(randomAnswer);
+    }
+    // finishes the creation of the set at 4 elements
+    if (buttons.size == 4) {
+      break;
+    }
+  }
+  // convert the SET "buttons" to ARRAY
+  buttons = Array.from(buttons);
+  // adds wrong answers to the rest of the buttons
+  for (var x = 1; x <= 4; x++) {
+    if (x != answerNumber) {
+      $(".button-" + x).text(buttons[x - 1]);
+    }
+  }
+}
+
+// checks to see wheter player was right or wrong
+function checkAnswer() {
+  $(".answer-button").on("click", function() {
+    userAnswer = this;
+    if (answer == parseInt($(userAnswer).text())) {
+      makeVisual("correct");
+      makeSound("correct");
+    } else {
+      makeVisual("incorrect");
+      makeSound("incorrect");
+    }
+    nextQuestion();
+  });
+}
+
+// continues to the next exercise
+function nextQuestion() {
+  $(".answer-button").off("click");
+  setTimeout(function() {
+    questionGenerator();
+  }, 600);
+}
+
+// makes the sound of wrong / right answers
+function makeSound(result) {
+
+  switch (result) {
+
+    case "correct":
+      var correctSound = new Audio("../sounds/correct.mp3");
+      correctSound.play();
+      break;
+
+    case "incorrect":
+      var incorrectSound = new Audio("../sounds/incorrect.mp3");
+      incorrectSound.play();
+      break;
+  }
+}
+
+// changes the visuals to match the user's wrong / right answer
+function makeVisual(result) {
+
+  switch (result) {
+
+    case "correct":
+      $(".question-card").addClass("question-card-correct");
+      setTimeout(function() {
+        $(".question-card").removeClass("question-card-correct");
+      }, 600);
+      $(userAnswer).addClass("answer-button-correct");
+      setTimeout(function() {
+        $(".answer-button-correct").removeClass("answer-button-correct");
+      }, 600);
+      break;
+
+    case "incorrect":
+      $(".question-card").addClass("question-card-incorrect");
+      setTimeout(function() {
+        $(".question-card").removeClass("question-card-incorrect");
+      }, 600);
+      $(userAnswer).addClass("answer-button-incorrect");
+      setTimeout(function() {
+        $(".answer-button-incorrect").removeClass("answer-button-incorrect");
+      }, 600);
+      break;
+
   }
 }
